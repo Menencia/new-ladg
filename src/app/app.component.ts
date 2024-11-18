eeadf4351658884587547f40c30d388928c99762
Menencia <lucien.stojcevski@gmail.com>
2024-11-18 22:33:42 +0100

    remove link on header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { Subscription } from 'rxjs';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
-import { DataService } from './shared/services/data.service';
 import { LangUtils } from './shared/utils/lang.utils';
 
 const DEFAULT_LANG = 'en';
@@ -15,17 +13,8 @@ const DEFAULT_LANG = 'en';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnDestroy {
-  lang = 'en';
-
-  private sub: Subscription;
-
-  constructor(
-    private translateService: TranslateService,
-    private dataService: DataService
-  ) {
-    this.sub = this.dataService.getLang().subscribe(lang => this.lang = lang);
-  }
+export class AppComponent implements OnInit {
+  constructor(private translateService: TranslateService) {}
 
   ngOnInit() {
     const savedLang = localStorage.getItem('lang');
@@ -33,9 +22,5 @@ export class AppComponent implements OnDestroy {
       savedLang,
       this.translateService.getBrowserLang()
     ));
-  }
-
-  ngOnDestroy() {
-    this.sub.unsubscribe();
   }
 }
